dd5278d80dcff75ca83d3d807ed51abc04a2d9e8
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-05-18 23:39:58 +0530

    Strivers code added to the half diamond

--- src/Patterns/Stars/HalfDiamond.java
+++ src/Patterns/Stars/HalfDiamond.java
@@ -2,7 +2,8 @@ package Patterns.Stars;
 
 public class HalfDiamond {
     public static void pattern10(int n) {
-        for (int i = 1; i <= n; i++) {
+        // 1st Method (Shradha)
+        /*for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
@@ -11,7 +12,6 @@ public class HalfDiamond {
             }
             System.out.println();
         }
-
         for (int i = n-1; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
@@ -20,11 +20,22 @@ public class HalfDiamond {
                 System.out.print(" ");
             }
             System.out.println();
+        }*/
+
+        // 2nd Method (Strivers)
+        for(int i = 1; i <= 2*n-1; i++){
+            int stars = i;
+            if(i > n) stars = 2*n-i;
+
+            for(int j = 1; j <= stars; j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
 
     }
     public static void main(String[] args) {
-        int n = 8;
+        int n = 4;
         pattern10(n);
     }
 }
