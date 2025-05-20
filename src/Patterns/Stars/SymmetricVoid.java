c9d3e97e9c00ba79febee9cdce352113785dfd51
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-05-20 23:59:17 +0530

    Symmetric Void Pattern

---
+++ src/Patterns/Stars/SymmetricVoid.java
@@ -0,0 +1,34 @@
+package Patterns.Stars;
+
+public class SymmetricVoid {
+    public static void pattern19(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < 2*i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n; i > 0; i--) {
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < 2*i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n = 6;
+        pattern19(n);
+    }
+}
