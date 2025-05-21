c9d539a01861bca2d09e02ea4798e7f3928c4dda
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-05-21 12:04:17 +0530

    changes in solid rectangle pattern

--- src/Patterns/Numbers/SolidRectangleNumber.java
+++ src/Patterns/Numbers/SolidRectangleNumber.java
@@ -5,17 +5,15 @@ public class SolidRectangleNumber {
         for(int i=0;i<2*n-1;i++){
             for(int j=0;j<2*n-1;j++){
                 // Initialising the top, down, left and right indices of a cell.
-                int top = i;
-                int bottom = j;
+                int bottom = (2*n - 2) - i;
                 int right = (2*n - 2) - j;
-                int left = (2*n - 2) - i;
-                System.out.print(n- Math.min(Math.min(top,bottom), Math.min(left,right)) + " ");
+                System.out.print(n - Math.min(Math.min(i,bottom), Math.min(j,right)) + " ");
             }
             System.out.println();
         }
     }
     public static void main(String[] args) {
-        int n = 4;
+        int n = 9;
         pattern22(n);
     }
 }
