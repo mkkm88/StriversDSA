d8e1b8be7a79f06e4e86b57f2becc4ebf80f6c5d
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-05-23 11:34:20 +0530

    Sum of first n numbers

---
+++ src/Recursion/SumOfN.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class SumOfN {
+    public static void sumOfNNumbers(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.print("The sum of first " + n + " numbers is : " + sum);
+
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = scanner.nextInt();
+
+        sumOfNNumbers(num);
+    }
+}
