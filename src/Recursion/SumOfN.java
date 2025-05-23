dd09bc1ba0644b92e30de0b851766d9678b067ac
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-05-23 11:34:47 +0530

    Sum of first n numbers using formula

--- src/Recursion/SumOfN.java
+++ src/Recursion/SumOfN.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class SumOfN {
     public static void sumOfNNumbers(int n) {
-        int sum = 0;
+        /*int sum = 0;
         for (int i = 1; i <= n; i++) {
             sum += i;
-        }
+        }*/
+        int sum = n*(n+1)/2;
         System.out.print("The sum of first " + n + " numbers is : " + sum);
 
     }
