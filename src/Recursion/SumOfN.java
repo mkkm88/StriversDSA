60033e13375d9d3fe8963de46739fd8c586518b1
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-05-23 12:14:03 +0530

    Sum of first n numbers using functional recursion

--- src/Recursion/SumOfN.java
+++ src/Recursion/SumOfN.java
@@ -3,7 +3,7 @@ package Recursion;
 import java.util.Scanner;
 
 public class SumOfN {
-    public static void sumOfNNumbers(int n, int sum) {
+    public static int sumOfNNumbers(int n) {
         // 1st Method : using the loop
         /*int sum = 0;
         for (int i = 1; i <= n; i++) {
@@ -14,19 +14,22 @@ public class SumOfN {
         /*int sum = n*(n+1)/2;
         System.out.print("The sum of first " + n + " numbers is : " + sum);*/
 
-        // 3rd Method : using recursion
-        if (n < 1) {
+        // 3rd Method : using parameterized recursion
+        /*if (n < 1) {
             System.out.print("The sum of numbers is : " + sum);
             return;
         }
-        sumOfNNumbers(n-1, sum+n);
+        sumOfNNumbers(n-1, sum+n);*/
 
+        // 4th Method : using functional recursion
+        if (n == 0) return 0;
+        return n + sumOfNNumbers(n-1);
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number : ");
         int num = scanner.nextInt();
 
-        sumOfNNumbers(num, 0);
+        System.out.println("The sum of first " + num + " numbers is : " + sumOfNNumbers(num));
     }
 }
