8f9ba75f9531e4956dd191e2c3c25c0795949a41
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-05-24 11:41:33 +0530

    Reverse an array using in-built library

--- src/Recursion/ReverseArrayRecursively.java
+++ src/Recursion/ReverseArrayRecursively.java
@@ -1,7 +1,11 @@
 package Recursion;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ReverseArrayRecursively {
-    public static void reverseArray(int[] arr, int start, int end) {
+    // Recursion Method
+    /*public static void reverseArray(int[] arr, int start, int end) {
         if (start < end) {
             int temp = arr[start];
             arr[start] = arr[end];
@@ -9,17 +13,29 @@ public class ReverseArrayRecursively {
             reverseArray(arr, start+1, end-1);
         }
     }
-
     public static void printArray(int[] arr, int n) {
         System.out.print("Reversed array is : ");
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i]+ " ");
         }
+    } */
+
+
+    // Using in-built library
+    public static void reverseArray(Integer[] arr) {
+        Collections.reverse(Arrays.asList(arr));
     }
+    public static void printArray(Integer[] arr, int n) {
+        System.out.print("Reversed array is : ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+ " ");
+        }
+    }
+
     public static void main(String[] args) {
         int n = 10;
-        int[] arr = {100,90,80,70,60,50,40,30,20,10};
-        reverseArray(arr, 0, n-1);
+        Integer[] arr = {0,1,2,3,4,5,6,7,8,9};
+        reverseArray(arr);
         printArray(arr, n);
     }
 }
