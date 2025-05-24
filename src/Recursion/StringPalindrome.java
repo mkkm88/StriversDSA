d407ce306bcc6e72da306a6977f011d724e66dca
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-05-25 00:17:46 +0530

    Checking a string is a palindrome or not using strivers approach

--- src/Recursion/StringPalindrome.java
+++ src/Recursion/StringPalindrome.java
@@ -6,12 +6,20 @@ public class StringPalindrome {
         int right = s.length() - 1;
         while (left < right){
             // 1st Method : Kasim
-            if (s.charAt(left) != s.charAt(right)) {
+            /*if (s.charAt(left) != s.charAt(right)) {
                 return false;
             } else {
                 left++;
                 right--;
-            }
+            }*/
+
+            // 2nd Method : Strivers Approach
+            char l= s.charAt(left);
+            char r = s.charAt(right);
+            if (!Character.isLetterOrDigit(l)) left++;
+            else if (!Character.isLetterOrDigit(r)) right--;
+            else if (Character.toLowerCase(l) != Character.toLowerCase(r)) return false;
+            else left++; right--;
         }
         return true;
     }
