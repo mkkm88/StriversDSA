7ab068e77b5a8f3bc27042bbb4995f5921c0763f
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-05-26 12:36:02 +0530

    Integer hashing

---
+++ src/Hashing/Hashing.java
@@ -0,0 +1,29 @@
+package Hashing;
+
+import java.util.Scanner;
+
+public class Hashing {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+
+        // input
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int[] hash = new int[13];
+        for (int i=0; i<size; i++) {
+            hash[arr[i]] += 1;
+        }
+
+        System.out.print("Enter the query length : ");
+        int q = scanner.nextInt();
+        while (q-- != 0) {
+            int num = scanner.nextInt();
+            System.out.println("The number found " + hash[num] + " times");
+        }
+    }
+}
