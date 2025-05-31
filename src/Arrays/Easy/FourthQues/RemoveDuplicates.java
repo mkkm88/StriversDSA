e0d2ba7db844e7c5ea43e773fece840fdd449949
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-05-31 12:11:36 +0530

    Remove Duplicates from an array

---
+++ src/Arrays/Easy/FourthQues/RemoveDuplicates.java
@@ -0,0 +1,21 @@
+package Arrays.Easy.FourthQues;
+
+public class RemoveDuplicates {
+    public static int removeDups(int[] arr) {
+        int i = 0;
+        for (int j=1; j<arr.length; j++) {
+            if (arr[i] != arr[j]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+        return i+1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {0,0,1,1,2,2,3,3,3,4,4,5,6,7,8};
+        int k = removeDups(arr);
+        for (int i=0; i<k; i++) {
+            System.out.print(arr[i]+ " ");
+        }
+    }
+}
