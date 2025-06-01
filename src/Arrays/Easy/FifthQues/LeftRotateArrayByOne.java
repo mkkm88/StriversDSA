40db404fd42802cb772f620282052a2779bfce90
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-06-01 16:31:52 +0530

    some comments

--- src/Arrays/Easy/FifthQues/LeftRotateArrayByOne.java
+++ src/Arrays/Easy/FifthQues/LeftRotateArrayByOne.java
@@ -6,24 +6,27 @@ public class LeftRotateArrayByOne {
     public static void leftRotateByOne(int[] arr) {
         // 1nd Method - TC => O(N) | SC => O(1)
         int n = arr.length;
-        int k = arr[0];
+        /*int k = arr[0];
         for (int i=0; i<n-1; i++) {
             arr[i] = arr[i+1];
         }
         arr[n-1] = k;
-        System.out.println(Arrays.toString(arr));
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }*/
 
 
         // 2nd Method - TC => O(N) | SC => O(N)
         // we use an extra array here to left shift
-        /*
         int[] temp = new int[n];
-        int n = arr.length;
+        //if (n - 1 >= 0) System.arraycopy(arr, 1, temp, 0, n - 1);
         for (int i=1; i<n; i++) {
             temp[i-1] = arr[i];
         }
         temp[n-1] = arr[0];
-        System.out.println(Arrays.toString(arr));*/
+        for (int i = 0; i < n; i++) {
+            System.out.print(temp[i]+" ");
+        }
     }
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
