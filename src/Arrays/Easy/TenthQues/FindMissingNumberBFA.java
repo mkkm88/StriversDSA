b368b5f3a983d13cede1bb2d34d754e268a13a17
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-06-08 15:29:58 +0530

    Find Missing number in the array

--- src/Arrays/Easy/TenthQues/FindMissingNumberBFA.java
+++ src/Arrays/Easy/TenthQues/FindMissingNumberBFA.java
@@ -3,8 +3,20 @@ package Arrays.Easy.TenthQues;
 public class FindMissingNumberBFA {
     public static void main(String[] args) {
         int n = 5;
+
+        // array of n-1 size
         int[] arr = {1,2,4,5};
 
+        // array of n size
+        int[] temp = {1,2,3,4,5};
 
+        int j = 0;
+        for (int i=0; i<n-1; i++) {
+            if (arr[i] != temp[j]) {
+                System.out.println("Missing number in the array is : "+ temp[j]);
+                break;
+            }
+            j++;
+        }
     }
 }
