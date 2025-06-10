5f21e72e6063134a3b14dcad18a3cd8fe1ff9206
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-06-10 19:04:03 +0530

    find number appears once in the array using linear search approach

---
+++ src/Arrays/Easy/NumberAppearOnce/BruteForceApproach.java
@@ -0,0 +1,24 @@
+package Arrays.Easy.NumberAppearOnce;
+
+public class BruteForceApproach {
+    public static int numberAppearOnce(int[] arr, int n) {
+        for (int i=0; i<n; i++) {
+            int num = arr[i];
+            int count = 0;
+            for (int j=0; j<n; j++) {
+                if (arr[j] == num) {
+                    count++;
+                }
+            }
+            if (count == 1) return num;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,2,1,1,3,4,6,5,6,3,4};
+        int n = arr.length;
+
+        int ans = numberAppearOnce(arr, n);
+        System.out.println("Number appears once in the array is : " + ans);
+    }
+}
