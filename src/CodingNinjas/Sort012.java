adce58357a250581ab11846f91c29e7b0f763f18
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-06-11 23:51:45 +0530

    sort array containing 0,1 and 2 using three pointer approach

--- src/CodingNinjas/Sort012.java
+++ src/CodingNinjas/Sort012.java
@@ -3,7 +3,7 @@ package CodingNinjas;
 import java.util.Arrays;
 
 public class Sort012 {
-    public static int[] sort012(int[] arr, int n) {
+    /*public static int[] sort012(int[] arr, int n) {
         int countZeros = 0;
         int countOnes = 0;
         int countTwos = 0;
@@ -30,9 +30,32 @@ public class Sort012 {
             }
         }
         return arr;
+    }*/
+    public static int[] sort012(int[] arr, int n) {
+        int current = 0;
+        int zeropos = 0;
+        int twopos = arr.length-1;
+
+        while (zeropos <= twopos) {
+            if (arr[zeropos] == 0) {
+                int temp = arr[current];
+                arr[current] = arr[zeropos];
+                arr[zeropos] = temp;
+                current++;
+                zeropos++;
+            } else if (arr[zeropos] == 1) {
+                zeropos++;
+            } else {
+                int temp1 = arr[zeropos];
+                arr[zeropos] = arr[twopos];
+                arr[twopos] = temp1;
+                twopos--;
+            }
+        }
+        return arr;
     }
     public static void main(String[] args) {
-        int[] arr = {0,1,2,2,1,0};
+        int[] arr = {0,1,2,2,1,0,2,1,0};
         int n = arr.length;
 
         /*Arrays.sort(arr);
