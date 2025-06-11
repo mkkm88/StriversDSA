921950e4683514aa286b92ea661b879fc1f49c1a
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-06-11 23:49:42 +0530

    sort the array containin 0,1 and 2

--- src/CodingNinjas/Sort012.java
+++ src/CodingNinjas/Sort012.java
@@ -3,11 +3,46 @@ package CodingNinjas;
 import java.util.Arrays;
 
 public class Sort012 {
+    public static int[] sort012(int[] arr, int n) {
+        int countZeros = 0;
+        int countOnes = 0;
+        int countTwos = 0;
+        for (int i=0; i<n; i++) {
+            if (arr[i] == 0) {
+                countZeros++;
+            } else if (arr[i] == 1) {
+                countOnes++;
+            } else {
+                countTwos++;
+            }
+        }
+
+        for (int i=0; i<n; i++) {
+            if(countZeros > 0) {
+                arr[i] = 0;
+                countZeros--;
+            } else if(countOnes > 0) {
+                arr[i] = 1;
+                countOnes--;
+            } else {
+                arr[i] = 2;
+                countTwos--;
+            }
+        }
+        return arr;
+    }
     public static void main(String[] args) {
         int[] arr = {0,1,2,2,1,0};
-        Arrays.sort(arr);
+        int n = arr.length;
+
+        /*Arrays.sort(arr);
         for (int j : arr) {
-            System.out.println(j);
+            System.out.print(j);
+        }*/
+
+        int[] ans = sort012(arr, n);
+        for (int j : ans) {
+            System.out.print(j+ " ");
         }
     }
 }
