877184dcdf1117def7ed54b912261f0e23a4579b
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-06-30 16:33:07 +0530

    Rearrange the array elements by sign

---
+++ src/Arrays/Medium/RearrangeElementsBySign/RearrangeElementsBFA.java
@@ -0,0 +1,37 @@
+package Arrays.Medium.RearrangeElementsBySign;
+import java.util.*;
+
+public class RearrangeElementsBFA {
+    public static int[] rearrangeBySign(int[] arr, int n){
+        ArrayList<Integer> pos=new ArrayList<>();
+        ArrayList<Integer> neg=new ArrayList<>();
+
+        // Segregate the array into positives and negatives.
+        for(int i=0;i<n;i++){
+
+            if(arr[i]>0) pos.add(arr[i]);
+            else neg.add(arr[i]);
+        }
+
+        // Positives on even indices, negatives on odd.
+        for(int i=0;i<n/2;i++){
+
+            arr[2*i] = pos.get(i);
+            arr[2*i+1] = neg.get(i);
+        }
+        return arr;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] arr= {1,2,-3,-1,-2,3};
+        int n = arr.length;
+
+        int[] ans= rearrangeBySign(arr,n);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(ans[i]+" ");
+        }
+
+    }
+}
