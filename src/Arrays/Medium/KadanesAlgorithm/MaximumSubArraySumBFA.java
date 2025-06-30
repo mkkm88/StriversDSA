581669d56f3dc613692dbd89cb902fecc7f276e0
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-06-30 16:01:03 +0530

    Maximum subarray sum optimal approach

--- src/Arrays/Medium/KadanesAlgorithm/MaximumSubArraySumBFA.java
+++ src/Arrays/Medium/KadanesAlgorithm/MaximumSubArraySumBFA.java
@@ -1,22 +1,39 @@
 package Arrays.Medium.KadanesAlgorithm;
 
 public class MaximumSubArraySumBFA {
-    public static int maximumSubArraySum(int[] arr, int n) {
-        int maxSum = Integer.MIN_VALUE;
+    public static long maximumSubArraySum(int[] arr, int n) {
+        long maxSum = Long.MIN_VALUE;
+        long sum = 0;
+
+        int start = 0;
+        int ansStart = -1, ansEnd = -1;
         for (int i=0; i<n; i++) {
-            int sum = 0;
-            for (int j=i; j<n; j++) {
-                sum += arr[j];
-                maxSum = Math.max(maxSum, sum);
+            if (sum == 0) start = i;
+            sum += arr[i];
+
+            if (sum > maxSum) {
+                maxSum = sum;
+
+                ansStart = start;
+                ansEnd = i;
             }
-        }g
-        return maxSum;
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        System.out.print("The subarray is : [");
+        for (int i=ansStart; i<=ansEnd; i++) {
+            System.out.print(" " + arr[i] + " ");
+        }
+        System.out.print("]\n");
+        return maxSum;g
     }
     public static void main(String[] args) {
-        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] arr = {-2, 1, 2, -1};
         int n = arr.length;
 
-        int ans = maximumSubArraySum(arr, n);
-        System.out.println(ans);
+        long maxSum = maximumSubArraySum(arr, n);
+        System.out.println("The maximum subarray sum is: " + maxSum);
     }
 }
