4583acc25feceebe83ee06bb7aa82bfa15b1c9fa
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-07-02 23:11:43 +0530

    Leaders in an array optimal approach

--- src/Arrays/Medium/LeadersInArray/OptimaApproach.java
+++ src/Arrays/Medium/LeadersInArray/OptimaApproach.java
@@ -2,20 +2,16 @@ package Arrays.Medium.LeadersInArray;
 
 import java.util.ArrayList;
 
-public class LeaderElementsInArray {
+public class OptimaApproach {
     public static ArrayList<Integer> printLeadersBruteForce(int[] arr, int n) {
         ArrayList<Integer> ans = new ArrayList<>();
 
-        for (int i=0; i<n; i++) {
-            boolean leader = true;
-            for (int j=i+1; j<n; j++) {
-                if (arr[j] > arr[i]) {
-                    leader = false;
-                    break;
-                }
-            }
-            if (leader) {
+        int max = arr[n-1];
+        ans.add(arr[n-1]);
+        for (int i=n-2; i>=0; i--) {
+            if (arr[i] > max) {
                 ans.add(arr[i]);
+                max = arr[i];
             }
         }
         return ans;
