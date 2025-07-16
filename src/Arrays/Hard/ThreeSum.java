73ffa82cf518e59f8c643518508def8865f1e4f2
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-07-16 13:08:42 +0530

    triplet sum optimal approach

--- src/Arrays/Hard/ThreeSum.java
+++ src/Arrays/Hard/ThreeSum.java
@@ -1,5 +1,6 @@
 package Arrays.Hard;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class ThreeSum {
@@ -18,7 +19,7 @@ public class ThreeSum {
             }
         }*/
 
-        for (int i=0; i<n; i++) {
+        /*for (int i=0; i<n; i++) {
             HashSet<Integer> hashset = new HashSet<>();
             for (int j=i+1; j<n; j++) {
                 int third = -(arr[i] + arr[j]);
@@ -29,11 +30,33 @@ public class ThreeSum {
                 }
                 hashset.add(arr[j]);
             }
+        }*/
+
+        Arrays.sort(arr);
+        for (int i=0; i<n; i++) {
+            if (i>0 && arr[i] == arr[i-1]) continue;
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int sum = arr[i] + arr[j] + arr[k];
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
+                    set.add(temp);
+                    j++;
+                    k--;
+                    while (j < k && arr[j] == arr[j-1]) j++;
+                    while (j < k && arr[k] == arr[k-1]) k--;
+                }
+            }
         }
         return new ArrayList<>(set);
     }
     public static void main(String[] args) {
-        int[] arr = {-1,0,2,-1,1,-4};
+        int[] arr = {0,-1,0,-2,2,2,0,-1,-2,2,-1,-2,2};
         List<List<Integer>> ans = triplet(arr);
         for (List<Integer> it : ans) {
             System.out.print("[");
