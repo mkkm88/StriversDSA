200c7552c4a98c8675a1d0f897d1632fd002ffd0
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-07-21 16:43:48 +0530

    Count of subarrays with xor K optimal approach

--- src/Arrays/Hard/CountSubArrayWithXORK.java
+++ src/Arrays/Hard/CountSubArrayWithXORK.java
@@ -1,5 +1,7 @@
 package Arrays.Hard;
 
+import java.util.HashMap;
+
 public class CountSubArrayWithXORK {
     public static int subarraysWithXORK(int[] arr, int k) {
         int n = arr.length;
@@ -14,11 +16,30 @@ public class CountSubArrayWithXORK {
             }
         }*/
 
-        for (int i=0; i<n; i++) {
+        /*for (int i=0; i<n; i++) {
             int xor = 0;
             for (int j=i; j<n; j++) {
                 xor = xor ^ arr[j];
                 if (xor == k) count++;
+            }
+        }*/
+
+        int xor = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(xor, 1);
+
+        for (int i=0; i<n; i++) {
+            xor = xor ^ arr[i];
+            int x = xor ^ k;
+
+            if (map.containsKey(x)) {
+                count += map.get(x);
+            }
+
+            if (map.containsKey(xor)) {
+                map.put(xor, map.get(xor) + 1);
+            } else {
+                map.put(xor, 1);
             }
         }
         return count;
