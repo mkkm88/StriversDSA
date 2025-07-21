ce676fd8c17185312350ac7a698e0c2118699952
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-07-21 22:52:56 +0530

    Maximum product subarray in an array better approach

--- src/Arrays/Hard/MaxProductSubArray.java
+++ src/Arrays/Hard/MaxProductSubArray.java
@@ -4,18 +4,16 @@ public class MaxProductSubArray {
     public static int maxProductSubArray(int[] arr, int n) {
         int max = Integer.MIN_VALUE;
         for (int i=0; i<n; i++) {
+            int prod = 1;
             for (int j=i+1; j<n; j++) {
-                int prod = 1;
-                for (int k=i; k<=j; k++) {
-                    prod *= arr[k];
-                    max = Math.max(max, prod);
-                }
+                prod *= arr[j];
+                max = Math.max(max, prod);
             }
         }
         return max;
     }
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,0};
+        int[] arr = {1,2,3,0,4,5,7};
         int n = arr.length;
 
         int ans = maxProductSubArray(arr, n);
