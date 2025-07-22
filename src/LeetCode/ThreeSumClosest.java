fb954e1ce9860b0df788489019a6617c5d5de146
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-07-22 12:17:04 +0530

    3Sum Closest Brute Force Approach little bit change in the code

--- src/LeetCode/ThreeSumClosest.java
+++ src/LeetCode/ThreeSumClosest.java
@@ -3,20 +3,18 @@ package LeetCode;
 public class ThreeSumClosest {
     public static int threeSumClosest(int[] arr, int n, int target) {
         int closest = arr[0] + arr[1] + arr[2];
-        int result = Integer.MAX_VALUE;
 
         for (int i=0; i<n-2; i++) {
             for (int j=i+1; j<n-1; j++) {
                 for (int k=j+1; k<n; k++) {
                     int sum = arr[i] + arr[j] + arr[k];
-                    if (Math.abs(sum - target) < closest) {
-                        closest = Math.abs(sum - target);
-                        result = sum;
+                    if (Math.abs(target - sum) < Math.abs(target - closest)) {
+                        closest = sum;
                     }
                 }
             }
         }
-        return result;
+        return closest;
     }
     public static void main(String[] args) {
         int[] arr = {-1,2,1,-4};
