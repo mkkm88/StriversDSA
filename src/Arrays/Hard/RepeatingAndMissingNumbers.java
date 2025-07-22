258a2ed443173dda353bd7537eac9d0be9293d51
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-07-22 14:32:54 +0530

    Find missing and repeating numbers in an array optimal approach usnig summation method

--- src/Arrays/Hard/RepeatingAndMissingNumbers.java
+++ src/Arrays/Hard/RepeatingAndMissingNumbers.java
@@ -1,10 +1,9 @@
 package Arrays.Hard;
-
 import java.util.Arrays;
 
 public class RepeatingAndMissingNumbers {
     public static int[] repeatingAndMissing(int[] arr, int n) {
-        int[] hash = new int[n+1];
+        /*int[] hash = new int[n+1];
 
         for (int i=0; i<n; i++) {
             hash[arr[i]]++;
@@ -17,10 +16,30 @@ public class RepeatingAndMissingNumbers {
 
             if (repeating != -1 && missing != -1) break;
         }
-        return new int[]{repeating, missing};
+        return new int[]{repeating, missing};*/
+
+        long SN = ((long) n *(n+1)) / 2;
+        long S2N = ((long) n * (n+1) *(2L *n+1)) / 6;
+        long S = 0;
+        long S2 = 0;
+
+        for (int i=0; i<n; i++) {
+            S += arr[i];
+            S2 += (long) arr[i] * (long) arr[i];
+        }
+
+        long val1 = S - SN;
+        long val2 = S2 - S2N;
+
+        val2 = val2 / val1;
+
+        long x = (val1 + val2) / 2;
+        long y = x - val1;
+
+        return new int[]{(int) x, (int) y};
     }
     public static void main(String[] args) {
-        int[] arr = {1,2,3,5,3};
+        int[] arr = {3,1,2,5,3};
         int n = arr.length;
 
         int[] ans = repeatingAndMissing(arr, n);
