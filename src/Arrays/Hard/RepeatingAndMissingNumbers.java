ec151e4e823f1d5337253812961e435871bc9477
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-07-22 14:43:09 +0530

    Find missing and repeating numbers in an array optimal approach usnig XOR  method

--- src/Arrays/Hard/RepeatingAndMissingNumbers.java
+++ src/Arrays/Hard/RepeatingAndMissingNumbers.java
@@ -18,7 +18,7 @@ public class RepeatingAndMissingNumbers {
         }
         return new int[]{repeating, missing};*/
 
-        long SN = ((long) n *(n+1)) / 2;
+        /*long SN = ((long) n *(n+1)) / 2;
         long S2N = ((long) n * (n+1) *(2L *n+1)) / 6;
         long S = 0;
         long S2 = 0;
@@ -36,7 +36,52 @@ public class RepeatingAndMissingNumbers {
         long x = (val1 + val2) / 2;
         long y = x - val1;
 
-        return new int[]{(int) x, (int) y};
+        return new int[]{(int) x, (int) y};*/
+
+
+        int xr = 0;
+        //Step 1: Find XOR of all elements:
+        for (int i = 0; i < n; i++) {
+            xr = xr ^ arr[i];
+            xr = xr ^ (i + 1);
+        }
+
+        //Step 2: Find the differentiating bit number:
+        int number = (xr & ~(xr - 1));
+
+        //Step 3: Group the numbers:
+        int zero = 0;
+        int one = 0;
+        for (int i = 0; i < n; i++) {
+            //part of 1 group:
+            if ((arr[i] & number) != 0) {
+                one = one ^ arr[i];
+            }
+            //part of 0 group:
+            else {
+                zero = zero ^ arr[i];
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            //part of 1 group:
+            if ((i & number) != 0) {
+                one = one ^ i;
+            }
+            //part of 0 group:
+            else {
+                zero = zero ^ i;
+            }
+        }
+
+        // Last step: Identify the numbers:
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == zero) cnt++;
+        }
+
+        if (cnt == 2) return new int[] {zero, one};
+        return new int[] {one, zero};
     }
     public static void main(String[] args) {
         int[] arr = {3,1,2,5,3};
