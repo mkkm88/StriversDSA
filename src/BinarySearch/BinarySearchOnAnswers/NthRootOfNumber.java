3d2a194a42d450743a646956ddd6a1fb3a3234a9
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-08-03 16:06:15 +0530

    Nth root of a number optimal approach

--- src/BinarySearch/BinarySearchOnAnswers/NthRootOfNumber.java
+++ src/BinarySearch/BinarySearchOnAnswers/NthRootOfNumber.java
@@ -1,30 +1,40 @@
 package BinarySearch.BinarySearchOnAnswers;
 
 public class NthRootOfNumber {
-    public static int func(int i, int n) {
-        int ans = 1;
-        while (n > 0) {
-            if (n % 2 == 1) {
-                ans = ans * i;
-                n = n-1;
-            } else {
-                i = i * i;
-                n = n / 2;
-            }
+    // return 1 if == m
+    // return 0 if < m
+    // return 2 if > m
+    public static int func(int n, int mid, int m) {
+        long  ans = 1;
+        for (int i=1; i<=n; i++) {
+            ans = ans * mid;
+            if (ans > m) return 2;
         }
-        return ans;
+        if (ans == m) return 1;
+        return 0;
     }
     public static int rootOfnumber(int n, int m) {
-        for (int i=1; i<m; i++) {
+        /*for (int i=1; i<m; i++) {
             if (func(i, n) == m) return i;
             else if (func(i, n) > m) {
                 break;
             }
         }
+        return -1;*/
+
+        int low = 1; int high = m;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (func(n, mid, m) == 1) return mid;
+            else if (func(n, mid, m) == 0) low = mid + 1;
+            else high = mid - 1;
+        }
         return -1;
     }
     public static void main(String[] args) {
-        int n = 3; int m = 69;
+        int n = 3; int m = 27;
         int result = 1;
 
         int ans = rootOfnumber(n, m);
